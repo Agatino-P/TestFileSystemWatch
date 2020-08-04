a0777b81a1f2569147bdb5334760dc9f90adf7f7
agatino.work@gmail.com <agatino.work@gmail.com>
2020-08-04 11:24:08 +0200

    Add project files.

---
+++ TestApp/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using GalaSoft.MvvmLight.Threading;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using TestFileSystemWatch;
+using TestFileSystemWatcher;
+
+namespace TestApp
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string _folder = @"D:\0_temp";
+        public string Folder { get => _folder; set { Set(() => Folder, ref _folder, value); }}
+        private string _extension = @".txt";
+        public string Extension { get => _extension; set { Set(() => Extension, ref _extension, value); }}
+
+        private int _timerMS = 1000;
+        public int TimerMS { get => _timerMS; set { Set(() => TimerMS, ref _timerMS, value); }}
+
+
+        private ObservableCollection<string> _changes = new ObservableCollection<string>();
+        public ObservableCollection<string> Changes { get => _changes; set { Set(() => Changes, ref _changes, value); }}
+        IFSysWatcher _fSysWatcher;
+
+        private RelayCommand _startCmd;
+        public RelayCommand StartCmd => _startCmd ?? (_startCmd = new RelayCommand(
+            () => start(),
+            () => _fSysWatcher == null,
+			keepTargetAlive:true
+            ));
+		private void start()
+        {
+            _fSysWatcher = new FSysWatcher(_folder, _extension, _timerMS,
+                (s) => DispatcherHelper.CheckBeginInvokeOnUI(()=> Messenger.Default.Send<string>(s, "FileSystemChange"))
+                );
+            _fSysWatcher.Start();
+        }
+
+        private RelayCommand _stopCmd;
+        public RelayCommand StopCmd => _stopCmd ?? (_stopCmd = new RelayCommand(
+            () => stop(),
+            () =>_fSysWatcher != null,
+			keepTargetAlive:true
+            ));
+		private void stop()
+        {
+            _fSysWatcher.Stop();
+            _fSysWatcher = null;
+        }
+
+
+        public MainWindowViewModel()
+        {
+            DispatcherHelper.Initialize();
+            Messenger.Default.Register<string>(this, "FileSystemChange", onFileSystemChange);
+        }
+
+        private void onFileSystemChange(string path)
+        {
+            Changes.Add($"{DateTime.Now.ToString()}: {path}");
+        }
+    }
+}
---
+++ TestFileSystemWatch/FSysWatcher.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Timers;
+using TestFileSystemWatch;
+
+namespace TestFileSystemWatcher
+{
+    public class FSysWatcher : IFSysWatcher
+    {
+        private int _timerMS;
+
+        private string _folder;
+        private string _extension;
+        private Action<string> _notifyAction;
+
+        private FileSystemWatcher fsWatcher;
+        private System.Timers.Timer fwTimer;
+        private List<string> fwPaths = new List<string>();
+
+
+        static private object lockObj = new object();
+
+        public FSysWatcher(string folder, string extension, int timerMS, Action<string> notifyAction)
+        {
+            _folder = folder;
+            _extension = extension;
+            _timerMS = timerMS;
+            _notifyAction = notifyAction;
+
+            fwTimer = new System.Timers.Timer(_timerMS);
+            fwTimer.Elapsed += onTimedEvent;
+            fwTimer.AutoReset = false;
+
+        }
+
+
+        public void Start()
+        {
+            try
+            {
+                if (fsWatcher == null)
+                {
+                    fsWatcher = new FileSystemWatcher(_folder, $"*{_extension}")
+                    {
+                        IncludeSubdirectories = true,
+                        NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.Size,
+                        EnableRaisingEvents = true
+                    };
+                    fsWatcher.Created += oneFileEvent;
+                    fsWatcher.Changed += oneFileEvent;
+                    fsWatcher.Deleted += oneFileEvent;
+                    fsWatcher.Renamed += twoFilesEvent;
+                }
+
+                fwTimer.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+            }
+
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                if (fsWatcher != null)
+                {
+                    fsWatcher.EnableRaisingEvents = false;
+                    fsWatcher.Created -= oneFileEvent;
+                    fsWatcher.Changed -= oneFileEvent;
+                    fsWatcher.Deleted -= oneFileEvent;
+                    fsWatcher.Renamed -= twoFilesEvent;
+                    fsWatcher.Dispose();
+                    fsWatcher = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+            }
+        }
+
+        private void oneFileEvent(object sender, FileSystemEventArgs e)
+        {
+            lock (lockObj)
+            {
+
+                if ((sender != null) && !fwPaths.Contains(e.FullPath))
+                {
+                    fwPaths.Add(e.FullPath);
+                    fwTimer.Start();
+                }
+            }
+        }
+
+        private void twoFilesEvent(object sender, RenamedEventArgs e)
+        {
+            lock (lockObj)
+            {
+                if ((sender != null) && !fwPaths.Contains(e.OldFullPath))
+                {
+                    fwPaths.Add(e.OldFullPath);
+                    fwTimer.Start();
+                }
+
+                if ((sender != null) && !fwPaths.Contains(e.FullPath))
+                {
+                    fwPaths.Add(e.FullPath);
+                    fwTimer.Start();
+                }
+            }
+
+        }
+
+        private void onTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            fwTimer.Stop();
+
+            lock (lockObj)
+            {
+                if (fwPaths.Count == 0)
+                {
+                    return;
+                }
+
+                while (fwPaths.Count > 0)
+                {
+                    if ((Path.GetExtension(fwPaths[0]).ToLower() == _extension.ToLower()))
+                    {
+                        try
+                        {
+                            _notifyAction(fwPaths[0]);
+                        }
+                        catch (Exception)
+                        {
+                        }
+                    }
+                    fwPaths.RemoveAt(0);
+                }
+            }
+
+            //fwTimer.Start();
+        }
+
+    }
+}
